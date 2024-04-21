7796c2736b8cdc0af75ae39c3c95a326f04f6645
914LauranDavid <david.lauran@stud.ubbcluj.ro>
2024-04-22 01:57:08 +0300

    use local db

--- library-manager/database/DBConnection.js
+++ library-manager/database/DBConnection.js
@@ -1,6 +1,9 @@
 const { MongoClient } = require("mongodb");
+const mongoose = require('mongoose');
 
 const connectionString = process.env.DB_ATLAS_URI || "";
+
+// mongoose.connect('mongodb://127.0.0.1:27017/CatApp', { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function connectToDatabase() {
     const client = new MongoClient(connectionString);
@@ -11,6 +14,11 @@ async function connectToDatabase() {
         throw e;
     }
     return conn.db("CatApp");
+    // const db = mongoose.connection;
+    // db.once('open', () => {
+    //     console.log('Connected to MongoDB');
+    // });
+    // return db;
 }
 
 module.exports = connectToDatabase;

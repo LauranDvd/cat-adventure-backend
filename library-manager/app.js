922a2c790f639abbdc88939ad488694fe2503579
914LauranDavid <david.lauran@stud.ubbcluj.ro>
2024-05-13 08:14:22 +0300

    some deploy

--- library-manager/app.js
+++ library-manager/app.js
@@ -49,4 +49,10 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
+// const PORT = process.env.PORT || 3000; // Use the PORT environment variable if available, otherwise default to 3000
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
 module.exports = app;

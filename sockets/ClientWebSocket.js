1651db68843e3650ba9319f450ce302acb3e9f23
914LauranDavid <david.lauran@stud.ubbcluj.ro>
2024-06-04 20:30:11 +0300

    wss for web socket

--- sockets/ClientWebSocket.js
+++ sockets/ClientWebSocket.js
@@ -1,6 +1,17 @@
 const { WebSocket } = require('ws');
+const https = require('https');
+const fs = require('fs');
 
-const wss = new WebSocket.Server({ port: 8087 });
+const options = {
+    key: fs.readFileSync('https/key.pem'),
+    cert: fs.readFileSync('https/cert.pem')
+};
+
+const httpsServer = https.createServer(options).listen(8087);
+
+const wss = new WebSocket.Server({ server: httpsServer });
+// const wss = new WebSocket('ws://localhost:8087');
+
 
 function sendSignal() {
     // console.log("Will send signal...");
